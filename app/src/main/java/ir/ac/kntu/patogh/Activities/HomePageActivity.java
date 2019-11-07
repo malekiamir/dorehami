9aba9c5cc09edfdd5d6432f8b111bd58699e0056
Amir <malekiamir@outlook.com>
2019-11-07 23:45:05 +0330

    search bar

--- app/src/main/java/ir/ac/kntu/patogh/Activities/HomePageActivity.java
+++ app/src/main/java/ir/ac/kntu/patogh/Activities/HomePageActivity.java
@@ -1,8 +1,10 @@
 package ir.ac.kntu.patogh.Activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,11 +24,18 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         ButterKnife.bind(this);
-        edtSearch.setOnFocusChangeListener((view, b) -> {
-            if (b) {
-                edtSearch.setHint(null);
-            }
-        });
+//        edtSearch.setOnFocusChangeListener((view, b) -> {
+//            if(view.getId() == edtSearch.getId()) {
+//                if (b) {
+//                    edtSearch.setHint(null);
+//                } else {
+//                    edtSearch.setHint(null);
+//                }
+//            }
+//        });
+
+        System.out.println();
+        edtSearch.setHint("what");
 
 //        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_homepage);
 //        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -49,5 +58,14 @@ public class HomePageActivity extends AppCompatActivity {
 //                return true;
 //            }
 //        });
+    }
+
+    public void searchBarClickHandler(View view) {
+        if (view.getId() == R.id.btn_img_search_bar_sort) {
+            Toast.makeText(this, "sort", Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_img_search_bar_search) {
+            Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }

5e6092a1e11e40776cdf956ad5d6ba2869e1e7c3
Amir <malekiamir@outlook.com>
2019-11-07 23:12:15 +0330

    search bar

--- app/src/main/java/ir/ac/kntu/patogh/Activities/HomePageActivity.java
+++ app/src/main/java/ir/ac/kntu/patogh/Activities/HomePageActivity.java
@@ -7,19 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ir.ac.kntu.patogh.R;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    @BindView(R.id.edt_home_page_search_bar)
+    EditText edtSearch;
+    @BindView(R.id.btn_img_search_bar_search)
+    EditText btnImgSearch;
+    @BindView(R.id.btn_img_search_bar_sort)
+    EditText btnImgSort;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
+        ButterKnife.bind(this);
+        edtSearch.setOnFocusChangeListener((view, b) -> {
+            if (b) {
+                edtSearch.setHint(null);
+            }
+        });
 
 //        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_homepage);
 //        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {

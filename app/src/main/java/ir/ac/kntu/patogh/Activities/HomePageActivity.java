9a22980b22b026097cd5479042e3bea99bc354a0
Amir <malekiamir@outlook.com>
2019-11-07 12:18:25 +0330

    bottom navigation update

--- app/src/main/java/ir/ac/kntu/patogh/Activities/HomePageActivity.java
+++ app/src/main/java/ir/ac/kntu/patogh/Activities/HomePageActivity.java
@@ -20,26 +20,26 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_homepage);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.action_add:
-                        Toast.makeText(HomePageActivity.this,"add event",Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_calendar:
-                        Toast.makeText(HomePageActivity.this,"this is calendar",Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_profile:
-                        Toast.makeText(HomePageActivity.this,"your profile",Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_home:
-                        Toast.makeText(HomePageActivity.this,"this is home page",Toast.LENGTH_SHORT).show();
-                        break;
-                }
-                return true;
-            }
-        });
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_homepage);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch(item.getItemId()){
+//                    case R.id.action_add:
+//                        Toast.makeText(HomePageActivity.this,"add event",Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.action_calendar:
+//                        Toast.makeText(HomePageActivity.this,"this is calendar",Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.action_profile:
+//                        Toast.makeText(HomePageActivity.this,"your profile",Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.action_home:
+//                        Toast.makeText(HomePageActivity.this,"this is home page",Toast.LENGTH_SHORT).show();
+//                        break;
+//                }
+//                return true;
+//            }
+//        });
     }
 }

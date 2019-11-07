42b97c08a755c59b8e3fcb8b1657bebbabfe6ff6
farzan64 <farzanfarahaniiii@gmail.com>
2019-11-07 10:25:11 +0330

    bottom navigation

---
+++ app/src/main/java/ir/ac/kntu/patogh/HomePageActivity.java
@@ -0,0 +1,44 @@
+package ir.ac.kntu.patogh;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.action_add:
+                        Toast.makeText(HomePageActivity.this,"add event",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_calendar:
+                        Toast.makeText(HomePageActivity.this,"this is calendar",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                        Toast.makeText(HomePageActivity.this,"your profile",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_home:
+                        Toast.makeText(HomePageActivity.this,"this is home page",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}

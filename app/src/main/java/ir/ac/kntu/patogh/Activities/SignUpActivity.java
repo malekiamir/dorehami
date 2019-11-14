6696d59fe362f566e6bbb68650711bdd183a7f67
farzan64 <farzanfarahaniiii@gmail.com>
2019-11-14 21:10:35 +0330

    sign up fielde validation updated

--- app/src/main/java/ir/ac/kntu/patogh/Activities/SignUpActivity.java
+++ app/src/main/java/ir/ac/kntu/patogh/Activities/SignUpActivity.java
@@ -2,17 +2,19 @@ package ir.ac.kntu.patogh.Activities;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewTreeObserver;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -25,53 +27,132 @@ import androidx.core.view.ViewCompat;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.muddzdev.styleabletoast.StyleableToast;
 
-import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import butterknife.BindView;
-import ir.ac.kntu.patogh.ApiDataTypes.TypeAuthentication;
-import ir.ac.kntu.patogh.ApiDataTypes.TypeEditUserDetails;
-import ir.ac.kntu.patogh.Interfaces.PatoghApi;
 import ir.ac.kntu.patogh.R;
 import jp.wasabeef.glide.transformations.BlurTransformation;
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class SignUpActivity extends AppCompatActivity {
 
     public static final String EXTRA_CIRCULAR_REVEAL_X = "EXTRA_CIRCULAR_REVEAL_X";
     public static final String EXTRA_CIRCULAR_REVEAL_Y = "EXTRA_CIRCULAR_REVEAL_Y";
 
-    @BindView(R.id.edt_signup_name)
-    TextInputLayout textInputLayoutName;
-    @BindView(R.id.edt_signup_surname)
-    TextInputLayout textInputLayoutSurname;
-    @BindView(R.id.edt_signup_email)
-    TextInputLayout textInputLayoutEmail;
+//    @BindView(R.id.edt_signup_name)
+//    EditText editTextName;
+//    @BindView(R.id.edt_signup_surname)
+//    EditText editTextSurname;
+//    @BindView(R.id.edt_signup_email)
+//    EditText editTextEmail;
 
 
     private int revealX;
     private int revealY;
     View rootLayout;
     private View view;
-    private String token;
-    boolean success = false;
+
+    private TextInputLayout textInputLayoutName;
+    private TextInputLayout textInputLayoutSurname;
+    private TextInputLayout textInputLayoutEmail;
+    private EditText editTextName;
+    private EditText editTextSurname;
+    private EditText editTextEmail;
+    private Button buttonSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         final Intent intent = getIntent();
-        rootLayout = findViewById(R.id.root_layout);
 
+        rootLayout = findViewById(R.id.root_layout);
+        textInputLayoutName = findViewById(R.id.textInputLayout_signup_namefield);
+        textInputLayoutSurname = findViewById(R.id.textInputLayout_signup_surnamefield);
+        textInputLayoutEmail = findViewById(R.id.textInputLayout_signup_email);
+        editTextName = findViewById(R.id.edt_signup_name);
+        editTextSurname = findViewById(R.id.edt_signup_surname);
+        editTextEmail = findViewById(R.id.edt_signup_email);
+        buttonSignUp = findViewById(R.id.btn_signup_register);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isSurnameValid() && isSurnameValid()
+                        &&isEmailValid(textInputLayoutEmail.getEditText().getText().toString().trim())){
+                    Intent intent = new Intent(SignUpActivity.this,HomePageActivity.class);
+                    startActivity(intent);
+                }
+                if (!(isNameValid())) {
+                    textInputLayoutName.setError("نام صحیح را وارد کنید.");
+
+                } else if (isNameValid()) {
+                        textInputLayoutName.setError(null);
+                }
+                if (!(isEmailValid(textInputLayoutEmail.getEditText().getText().toString().trim()))){
+                    textInputLayoutEmail.setError("ایمیل صحیح را وارد کنید.");
+                }else if(isEmailValid(textInputLayoutEmail.getEditText().getText().toString().trim())){
+                    textInputLayoutEmail.setError(null);
+                }
+                if (!(isSurnameValid())){
+                    textInputLayoutSurname.setError("نام خانوادگی صحیح را وارد کنید.");
+                }else if(isSurnameValid()){
+                    textInputLayoutSurname.setError(null);
+                }
+            }
+        });
+
+//        editTextName.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if(i1>textInputLayoutName.getCounterMaxLength()){
+//                    textInputLayoutName.setError("نام طولانی است.");
+//                }else if(i1<2){
+//                    textInputLayoutName.setError("نام کوتاه است.");
+//                }else{
+//                    textInputLayoutName.setErrorEnabled(false);
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+//
+//        editTextSurname.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if(i1>textInputLayoutSurname.getCounterMaxLength()){
+//                    textInputLayoutSurname.setError("نام خانوادگی طولانی است.");
+//                }else if(i1<2){
+//                    textInputLayoutSurname.setError("نام خانوادگی کوتاه است.");
+//                }else{
+//                    textInputLayoutSurname.setErrorEnabled(false);
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+
+
+        editTextName.addTextChangedListener(signInTextWatcher);
+        editTextSurname.addTextChangedListener(signInTextWatcher);
+        editTextEmail.addTextChangedListener(signInTextWatcher);
 
         if (savedInstanceState == null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
                 intent.hasExtra(EXTRA_CIRCULAR_REVEAL_X) &&
@@ -98,6 +179,28 @@ public class SignUpActivity extends AppCompatActivity {
         }
 
     }
+
+    private TextWatcher signInTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String signUpName = editTextName.getText().toString().trim();
+            String signUpSurname = editTextSurname.getText().toString().trim();
+            String signUpEmail = editTextEmail.getText().toString().trim();
+
+
+            buttonSignUp.setEnabled(!signUpName.isEmpty() && !signUpSurname.isEmpty() && !signUpEmail.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
     @Override
     protected void onDestroy() {
@@ -146,106 +249,38 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
 
-    private boolean checkEmailAddress() {
-        String EmailInput = textInputLayoutEmail.getEditText().getText().toString().trim();
-        if (EmailInput.equals("")) {
-            textInputLayoutEmail.setError("این فیلد نمیتواند خالی باشد.");
-            return false;
-        } else if (!(EmailInput.contains("@"))) {
-            textInputLayoutEmail.setError("لطفاآدرس ایمیل رامجددا وارد کنید.");
-            return false;
-        } else {
-            textInputLayoutEmail.setError(null);
-            return true;
+    public static boolean isEmailValid(String Email) {
+        boolean isValid = true;
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        CharSequence inputStr = Email;
+
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+        if (!matcher.matches()) {
+            isValid = false;
         }
+        return isValid;
     }
 
-    private boolean checkName(){
-        String NameInput = textInputLayoutName.getEditText().getText().toString().trim();
-        if (NameInput.equals("")) {
-            textInputLayoutEmail.setError("این فیلد نمیتواند خالی باشد.");
-            return false;
-        }else if(NameInput.length()>20){
-            textInputLayoutName.setError("لطفا نام معتبر را وارد کنید.");
-            return false;
-        }else if(NameInput.length()<3){
-            textInputLayoutName.setError("لطفا نام معتبر را وارد کنید.");
-            return false;
-        }else {
-            textInputLayoutName.setError(null);
-            return true;
+    public boolean isNameValid() {
+        boolean isValid = true;
+        if (textInputLayoutName.getEditText().getText().toString().trim().length() > 20 ||
+                textInputLayoutName.getEditText().getText().toString().trim().length()< 3) {
+            isValid = false;
         }
+        return isValid;
     }
 
-    private boolean checkSurname(){
-        String SurnameInput = textInputLayoutSurname.getEditText().getText().toString().trim();
-        if (SurnameInput.equals("")) {
-            textInputLayoutEmail.setError("این فیلد نمیتواند خالی باشد.");
-            return false;
-        }else if(SurnameInput.length()>45){
-            textInputLayoutName.setError("لطفا نام خانوادگی معتبر را وارد کنید.");
-            return false;
-        }else if(SurnameInput.length()<3){
-            textInputLayoutName.setError("لطفا نام خانوادگی معتبر را وارد کنید.");
-            return false;
-        }else {
-            textInputLayoutName.setError(null);
-            return true;
+    public boolean isSurnameValid() {
+        boolean isValid = true;
+        if (textInputLayoutSurname.getEditText().getText().toString().trim().length() > 40 ||
+                textInputLayoutSurname.getEditText().getText().toString().trim().length() < 3) {
+            isValid = false;
         }
+        return isValid;
     }
 
-    public void clickHandler(View view) {
-        if (view.getId() == R.id.btn_signup_register) {
-            if (checkSurname() && checkName() && checkEmailAddress()) {
-                Intent intent = new Intent(SignUpActivity.this, HomePageActivity.class);
-                startActivity(intent);
-            }
-        }
-    }
 
-    private boolean editUserDetails() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://185.252.30.32:7700/api/")
-                .build();
-        Gson gson = new Gson();
-        PatoghApi patoghApi = retrofit.create(PatoghApi.class);
-        String phoneNumber = getIntent().getStringExtra("phoneNumber");
-        String token = getIntent().getStringExtra("token");
-
-        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json")
-                , gson.toJson(new TypeEditUserDetails(phoneNumber
-                        , textInputLayoutName.getEditText().toString()
-                        , textInputLayoutSurname.getEditText().toString()
-                        , textInputLayoutEmail.getEditText().toString())
-                ));
-
-        patoghApi.editUserDetails(requestBody).enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                try {
-                    Response<ResponseBody> saveResponse = response;
-                    String responseBody = response.body().string();
-                    Log.d("~~~~~~~~~~~~~~~~~", responseBody);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                success = true;
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                new StyleableToast
-                        .Builder(SignUpActivity.this)
-                        .text("لطفا اتصال اینترنت را بررسی نمایید و سپس مجددا تلاش نمایید.")
-                        .textColor(Color.WHITE)
-                        .backgroundColor(Color.argb(255, 255, 94, 100))
-                        .show();
-                success = false;
-            }
-        });
-
-        return success;
-    }
 
 }

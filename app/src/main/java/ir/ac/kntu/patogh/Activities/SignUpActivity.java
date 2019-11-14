e65aaae627e286d0f81a59c954500a34e8404aa7
farzan64 <farzanfarahaniiii@gmail.com>
2019-11-14 13:48:28 +0330

    sign up fields validation fixed

--- app/src/main/java/ir/ac/kntu/patogh/Activities/SignUpActivity.java
+++ app/src/main/java/ir/ac/kntu/patogh/Activities/SignUpActivity.java
@@ -2,13 +2,19 @@ package ir.ac.kntu.patogh.Activities;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewTreeObserver;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -22,6 +28,9 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import butterknife.BindView;
 import ir.ac.kntu.patogh.R;
 import jp.wasabeef.glide.transformations.BlurTransformation;
@@ -31,18 +40,23 @@ public class SignUpActivity extends AppCompatActivity {
     public static final String EXTRA_CIRCULAR_REVEAL_X = "EXTRA_CIRCULAR_REVEAL_X";
     public static final String EXTRA_CIRCULAR_REVEAL_Y = "EXTRA_CIRCULAR_REVEAL_Y";
 
-    @BindView(R.id.edt_signup_name)
-    TextInputLayout textInputLayoutName;
-    @BindView(R.id.edt_signup_surname)
-    TextInputLayout textInputLayoutSurname;
-    @BindView(R.id.edt_signup_email)
-    TextInputLayout textInputLayoutEmail;
+//    @BindView(R.id.edt_signup_name)
+//    EditText editTextName;
+//    @BindView(R.id.edt_signup_surname)
+//    EditText editTextSurname;
+//    @BindView(R.id.edt_signup_email)
+//    EditText editTextEmail;
 
 
     private int revealX;
     private int revealY;
     View rootLayout;
     private View view;
+
+    private EditText editTextName;
+    private EditText editTextSurname;
+    private EditText editTextEmail;
+    private Button buttonSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +65,14 @@ public class SignUpActivity extends AppCompatActivity {
         final Intent intent = getIntent();
 
         rootLayout = findViewById(R.id.root_layout);
+        editTextName = findViewById(R.id.edt_signup_name);
+        editTextSurname = findViewById(R.id.edt_signup_surname);
+        editTextEmail = findViewById(R.id.edt_signup_email);
+        buttonSignUp = findViewById(R.id.btn_signup_register);
 
+        editTextName.addTextChangedListener(signInTextWatcher);
+        editTextSurname.addTextChangedListener(signInTextWatcher);
+        editTextEmail.addTextChangedListener(signInTextWatcher);
 
         if (savedInstanceState == null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
                 intent.hasExtra(EXTRA_CIRCULAR_REVEAL_X) &&
@@ -78,6 +99,28 @@ public class SignUpActivity extends AppCompatActivity {
         }
 
     }
+
+    private TextWatcher signInTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String signUpName = editTextName.getText().toString().trim();
+            String signUpSurname = editTextSurname.getText().toString().trim();
+            String signUpEmail = editTextEmail.getText().toString().trim();
+
+
+            buttonSignUp.setEnabled(!signUpName.isEmpty() && !signUpSurname.isEmpty() && !signUpEmail.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
     @Override
     protected void onDestroy() {
@@ -126,61 +169,57 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
 
-    private boolean checkEmailAddress() {
-        String EmailInput = textInputLayoutEmail.getEditText().getText().toString().trim();
-        if (EmailInput.equals("")) {
-            textInputLayoutEmail.setError("این فیلد نمیتواند خالی باشد.");
-            return false;
-        } else if (!(EmailInput.contains("@"))) {
-            textInputLayoutEmail.setError("لطفاآدرس ایمیل رامجددا وارد کنید.");
-            return false;
-        } else {
-            textInputLayoutEmail.setError(null);
-            return true;
+    public static boolean isEmailValid(String editTextEmail) {
+        boolean isValid = true;
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        CharSequence inputStr = editTextEmail;
+
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+        if (!matcher.matches()) {
+            isValid = false;
         }
+        return isValid;
     }
 
-    private boolean checkName(){
-        String NameInput = textInputLayoutName.getEditText().getText().toString().trim();
-        if (NameInput.equals("")) {
-            textInputLayoutEmail.setError("این فیلد نمیتواند خالی باشد.");
-            return false;
-        }else if(NameInput.length()>20){
-            textInputLayoutName.setError("لطفا نام معتبر را وارد کنید.");
-            return false;
-        }else if(NameInput.length()<3){
-            textInputLayoutName.setError("لطفا نام معتبر را وارد کنید.");
-            return false;
-        }else {
-            textInputLayoutName.setError(null);
-            return true;
+    public boolean isNameValid() {
+        boolean isValid = true;
+        // String NameInput = editTextName.getText().toString().trim();
+        if (editTextName.length() > 20 || editTextName.length() < 3) {
+            isValid = false;
         }
+        return isValid;
     }
 
-    private boolean checkSurname(){
-        String SurnameInput = textInputLayoutSurname.getEditText().getText().toString().trim();
-        if (SurnameInput.equals("")) {
-            textInputLayoutEmail.setError("این فیلد نمیتواند خالی باشد.");
-            return false;
-        }else if(SurnameInput.length()>45){
-            textInputLayoutName.setError("لطفا نام خانوادگی معتبر را وارد کنید.");
-            return false;
-        }else if(SurnameInput.length()<3){
-            textInputLayoutName.setError("لطفا نام خانوادگی معتبر را وارد کنید.");
-            return false;
-        }else {
-            textInputLayoutName.setError(null);
-            return true;
+    public boolean isSurnameValid() {
+        boolean isValid = true;
+        // String NameInput = editTextName.getText().toString().trim();
+        if (editTextSurname.length() > 40 || editTextSurname.length() < 3) {
+            isValid = false;
         }
+        return isValid;
     }
 
+    @TargetApi(Build.VERSION_CODES.P)
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void clickHandler(View view) {
         if (view.getId() == R.id.btn_signup_register) {
-            if (checkSurname() && checkName() && checkEmailAddress()) {
+            String signUpEmail = editTextEmail.getText().toString().trim();
+            if (isEmailValid(signUpEmail) && isNameValid() && isSurnameValid()) {
                 Intent intent = new Intent(SignUpActivity.this, HomePageActivity.class);
                 startActivity(intent);
+            } else if (!(isNameValid())) {
+                Toast.makeText(this, "لطفا نام را درست واردکنید.", Toast.LENGTH_LONG).show();
+                editTextName.setTextColor(Color.rgb(247, 17, 5));
+            } else if (!(isSurnameValid())) {
+                Toast.makeText(this, "لطفا نام خانوادگی را درست واردکنید.", Toast.LENGTH_LONG).show();
+                editTextName.setTextColor(Color.rgb(247, 17, 5));
+            } else if (!(isEmailValid(signUpEmail))) {
+                Toast.makeText(this, "لطفاآدرس ایمیل را درست واردکنید.", Toast.LENGTH_LONG).show();
+                editTextEmail.setTextColor(Color.rgb(247, 17, 5));
             }
         }
     }
+
 }

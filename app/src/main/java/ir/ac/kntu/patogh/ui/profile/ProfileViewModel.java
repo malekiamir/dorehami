a8e3b2840ff6f23b9e69883ac754af2c106f5795
Amir <malekiamir@outlook.com>
2019-11-16 11:38:30 +0330

    fragment profile coming soon

--- app/src/main/java/ir/ac/kntu/patogh/ui/profile/ProfileViewModel.java
+++ app/src/main/java/ir/ac/kntu/patogh/ui/profile/ProfileViewModel.java
@@ -6,14 +6,8 @@ import androidx.lifecycle.ViewModel;
 
 public class ProfileViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
 
     public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
 }

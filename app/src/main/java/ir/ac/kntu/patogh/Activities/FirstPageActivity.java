0aafee2b1f6e1a03a004c837db1406c6710bfee2
farzan64 <farzanfarahaniiii@gmail.com>
2019-11-29 00:43:31 +0330

    type writer

--- app/src/main/java/ir/ac/kntu/patogh/Activities/FirstPageActivity.java
+++ app/src/main/java/ir/ac/kntu/patogh/Activities/FirstPageActivity.java
@@ -2,43 +2,70 @@ package ir.ac.kntu.patogh.Activities;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
-import butterknife.BindView;
 import ir.ac.kntu.patogh.R;
-
-import static ir.ac.kntu.patogh.R.drawable.avd_anim;
 
 public class FirstPageActivity extends AppCompatActivity {
 
-//    @BindView(R.id.img_first_page_animation)
+    //    @BindView(R.id.img_first_page_animation)
 //    ImageView imageView;
     private AnimatedVectorDrawable animatedVectorDrawable;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
         ImageView imageView = findViewById(R.id.img_first_page_animation);
+        Typewriter writer = findViewById(R.id.tv_firstPage_animated_text);
         Animatable animatable = (Animatable) imageView.getDrawable();
-        animatable.start();
-//        AnimatedVectorDrawable animatedLoaderDrawable = (AnimatedVectorDrawable) getDrawable(avd_anim);
-//        imageView.setImageDrawable(animatedLoaderDrawable);
-//        animatedLoaderDrawable.start();
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            animatedVectorDrawable = (AnimatedVectorDrawable) getDrawable(avd_anim);
+        // Animation animation = imageView.getDrawable();
+        //animatable.start();
+//        new Thread(){
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(1200);
+//                    writer.setText("");
+//                    writer.setCharacterDelay(120);
+//                    writer.animateText("پاتوق");
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }.start();
+//        Thread anim = new Thread((Runnable) animatable);
+//        anim.start();
+//        try {
+//            anim.sleep(1200);
+//            writer.setText("");
+//            writer.setCharacterDelay(120);
+//            writer.animateText("پاتوق");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
 //        }
-//        AnimatedVectorDrawableCompat animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(this, avd_anim);
-////        imageView.setImageDrawable(R.drawable.avd_anim);
-//        Animatable animatable = (Animatable) imageView.getDrawable();
-//        animatable.start();
-////        Animatable animatable = (Animatable) imageView.getDrawable(R.drawable.avd_anim);
-////        animatable.start();
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1200);
+                    writer.setText("");
+                    writer.setCharacterDelay(120);
+                    writer.animateText("پاتوق");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        animatable.start();
+        thread.start();
     }
 }

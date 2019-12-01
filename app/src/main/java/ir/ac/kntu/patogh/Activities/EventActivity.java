9b77cda813696b56710730ff6778f9c8bff17d0f
Amir <malekiamir@outlook.com>
2019-12-01 21:34:43 +0330

    map add

--- app/src/main/java/ir/ac/kntu/patogh/Activities/EventActivity.java
+++ app/src/main/java/ir/ac/kntu/patogh/Activities/EventActivity.java
@@ -1,20 +1,32 @@
 package ir.ac.kntu.patogh.Activities;
 
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.neshan.core.LngLat;
+import org.neshan.layers.VectorElementLayer;
+import org.neshan.services.NeshanMapStyle;
+import org.neshan.services.NeshanServices;
+import org.neshan.styles.AnimationStyle;
+import org.neshan.styles.AnimationStyleBuilder;
+import org.neshan.styles.AnimationType;
+import org.neshan.styles.MarkerStyle;
+import org.neshan.styles.MarkerStyleCreator;
+import org.neshan.ui.MapView;
+import org.neshan.utils.BitmapUtils;
+import org.neshan.vectorelements.Marker;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +40,9 @@ public class EventActivity extends AppCompatActivity {
     TextView tvDesc;
     @BindView(R.id.event_date)
     TextView tvDate;
+    @BindView(R.id.map)
+    MapView map;
+    VectorElementLayer markerLayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +65,38 @@ public class EventActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        LngLat focalPoint = new LngLat(51.336434, 35.6990015);
+        map.setFocalPointPosition(focalPoint, 0f);
+        map.setZoom(15,2);
+        markerLayer = NeshanServices.createVectorElementLayer();
+        map.getLayers().add(NeshanServices.createBaseMap(NeshanMapStyle.STANDARD_DAY));
+        map.getLayers().add(markerLayer);
+
+        markerLayer.clear();
+
+        AnimationStyleBuilder animStBl = new AnimationStyleBuilder();
+        animStBl.setFadeAnimationType(AnimationType.ANIMATION_TYPE_SMOOTHSTEP);
+        animStBl.setSizeAnimationType(AnimationType.ANIMATION_TYPE_SPRING);
+        animStBl.setPhaseInDuration(0.5f);
+        animStBl.setPhaseOutDuration(0.5f);
+        AnimationStyle animSt = animStBl.buildStyle();
+
+        // Creating marker style. We should use an object of type MarkerStyleCreator, set all features on it
+        // and then call buildStyle method on it. This method returns an object of type MarkerStyle
+        MarkerStyleCreator markStCr = new MarkerStyleCreator();
+        markStCr.setSize(20f);
+        markStCr.setBitmap(BitmapUtils.createBitmapFromAndroidBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_marker)));
+        // AnimationStyle object - that was created before - is used here
+        markStCr.setAnimationStyle(animSt);
+        MarkerStyle markSt = markStCr.buildStyle();
+
+        // Creating marker
+        Marker marker = new Marker(focalPoint, markSt);
+
+        // Adding marker to markerLayer, or showing marker on map!
+        markerLayer.add(marker);
+
+
     }
 
     @Override

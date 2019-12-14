ceac39384f08916d1fa48268447fde24e58049f7
Amir <malekiamir@outlook.com>
2019-12-14 16:20:18 +0330

    animation min time

--- app/src/main/java/ir/ac/kntu/patogh/Activities/FirstPageActivity.java
+++ app/src/main/java/ir/ac/kntu/patogh/Activities/FirstPageActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Build;
@@ -11,66 +12,98 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.widget.ImageView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import ir.ac.kntu.patogh.Interfaces.PatoghApi;
 import ir.ac.kntu.patogh.R;
 import ir.ac.kntu.patogh.Utils.Typewriter;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class FirstPageActivity extends AppCompatActivity {
 
-    //    @BindView(R.id.img_first_page_animation)
-//    ImageView imageView;
-    private AnimatedVectorDrawable animatedVectorDrawable;
+    private SharedPreferences sharedPreferences;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
-        ImageView imageView = findViewById(R.id.img_first_page_animation);
-        Typewriter writer = findViewById(R.id.tv_firstPage_animated_text);
-        Animatable animatable = (Animatable) imageView.getDrawable();
-        // Animation animation = imageView.getDrawable();
-        //animatable.start();
-//        new Thread(){
-//            @Override
-//            public void run() {
-//                try {
-//                    Thread.sleep(1200);
-//                    writer.setText("");
-//                    writer.setCharacterDelay(120);
-//                    writer.animateText("پاتوق");
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }.start();
-//        Thread anim = new Thread((Runnable) animatable);
-//        anim.start();
-//        try {
-//            anim.sleep(1200);
-//            writer.setText("");
-//            writer.setCharacterDelay(120);
-//            writer.animateText("پاتوق");
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        sharedPreferences = getApplicationContext()
+                .getSharedPreferences("TokenPref",0);
 
+        Typewriter writer = findViewById(R.id.tv_firstPage_animated_text);
         writer.setText("");
         writer.setCharacterDelay(120);
         writer.animateText("پاتوق");
-        Thread thread = new Thread() {
+        checkValidToken();
+
+    }
+
+    private void checkValidToken() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://eg.potatogamers.ir:7701/api/")
+                .build();
+        long t1 = System.currentTimeMillis();
+        PatoghApi patoghApi = retrofit.create(PatoghApi.class);
+        String token = sharedPreferences.getString("Token", "none");
+        if (token.equals("none")) {
+            Intent intent = new Intent(FirstPageActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        patoghApi.getUserDetails("Bearer " + token).enqueue(new Callback<ResponseBody>() {
             @Override
-            public void run() {
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 try {
-                    sleep(3000);
-                    Intent intent = new Intent(FirstPageActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
+                    System.out.println(response.body());
+                    String res = response.body().string();
+                    System.out.println(res);
+                    JsonObject jsonObject1 = new Gson().fromJson(res, JsonObject.class);
+                    String returnValue = jsonObject1.get("returnValue").toString();
+                    JsonObject jsonObject2 = new Gson().fromJson(returnValue, JsonObject.class);
+                    String firstName = jsonObject2.get("firstName").getAsString();
+                    if(firstName != null) {
+                        long t2 = System.currentTimeMillis();
+                        if(t2-t1 >= 1450) {
+                            Intent intent = new Intent(FirstPageActivity.this, HomePageActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            new Thread()  {
+                                @Override
+                                public void run() {
+                                    try {
+                                        sleep(1450-t2+t1);
+                                        Intent intent = new Intent(FirstPageActivity.this, HomePageActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }.start();
+                        }
+                    } else {
+                        Intent intent = new Intent(FirstPageActivity.this, SignUpActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
+                    System.out.println("exception");
                 }
             }
-        };
 
-        thread.start();
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+            }
+        });
     }
 }
